50442c948c5738ab59b4e83a6744d6b3bdec4e03
whdhyuck <zpkv25@gmail.com>
2021-07-29 16:34:08 +0900

    수정

--- loop_2.js
+++ loop_2.js
@@ -2,24 +2,38 @@ console.log("1번")
 let sum = 0;
 for (i=1; i<=100; i++) {
     sum += i;
-    console.log(sum)
 }
+console.log(sum)
 
 console.log("2번")
-let a = 1;
-for (i=0; i<=5; i++) {
-    a += i;
+let a =''
+for (i=1; i<=5; i++){
+    a += i + ' '
     console.log(a)
 }
 
 console.log("3번")
 for(x=1; x<=9; x++) {
     for (i=1; i<=9; i++) {
-     console.log(x + '*' + i + '=' + x*i)
+     console.log(x + ' * ' + i + ' = ' + x*i)
     }
-
 }
 
-
-
 console.log("4번")
+const readline = require('readline')
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdin.stdout
+})
+console.log("1. 내 나이는 ?")
+console.log("2. 내 성별은 ?")
+rl.on('line', line => {
+    if (line == '1') {
+        console.log("24")
+    } else if (line =='2') {
+        console.log("남자")
+    } else {
+        console.log("오류")
+    }
+    process.exit()
+})
